2a9f0b28c0267c4a46e91faf3e36b36b0102bbd6
Daniel <daniel@kylix.online>
2022-08-05 09:15:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const h1 = document.createElement("h1");
+h1.textContent = "My name is Daniel, I am a champion!!"
